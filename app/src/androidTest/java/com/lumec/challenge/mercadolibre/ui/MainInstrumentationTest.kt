e0e53db3a8fe045b94e4f997d8bd8390f95a6680
lumec <lumec@engineer.com>
2023-02-13 12:14:48 -0500

    UI Testing

--- app/src/androidTest/java/com/lumec/challenge/mercadolibre/ui/MainInstrumentationTest.kt
+++ app/src/androidTest/java/com/lumec/challenge/mercadolibre/ui/MainInstrumentationTest.kt
@@ -1,21 +1,32 @@
 package com.lumec.challenge.mercadolibre.ui
 
+import android.util.Log
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.lumec.challenge.data.ProductRemoteDataSource
 import com.lumec.challenge.mercadolibre.MainActivity
 import com.lumec.challenge.mercadolibre.data.server.CoroutinesTestRule
+import com.lumec.challenge.mercadolibre.data.server.ErrorDispatcher
 import com.lumec.challenge.mercadolibre.data.server.MockWebServerRule
-import com.lumec.challenge.mercadolibre.data.server.fromJson
+import com.lumec.challenge.mercadolibre.data.server.SuccessDispatcher
+import com.lumec.challenge.mercadolibre.ui.theme.MercadoLibreTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import okhttp3.mockwebserver.MockResponse
+import okhttp3.OkHttpClient
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import timber.log.Timber
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
@@ -32,42 +43,154 @@ class MainInstrumentationTest {
     val mockWebServerRule = MockWebServerRule()
 
     @get:Rule(order = 3)
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Inject
     lateinit var remoteDataSource: ProductRemoteDataSource
 
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+
     @Before
     fun setUp() {
         hiltRule.inject()
+
+        IdlingRegistry.getInstance().resources.forEach {
+            Timber.e("resource ${it.name}    idleNow: ${it.isIdleNow}")
+            if (it.name == "Compose-Espresso link") {
+                IdlingRegistry.getInstance().unregister(it)
+            }
+        }
     }
 
     @Test
     fun getProductsByNameResult_sentRequest_receivedExpected() = runTest {
-        val mockResponse = MockResponse()
-        mockResponse.setResponseCode(200)
-        mockWebServerRule.server.enqueue(mockResponse.fromJson("product_list.json"))
+        mockWebServerRule.server.dispatcher = SuccessDispatcher()
 
+        testDispatcher.advanceUntilIdle()
         val productList = remoteDataSource.getProductsByName("Motorola G6")
 
         productList.fold({ throw Exception(it.toString()) }) {
+            Assert.assertEquals("MLA810645375", it[0].id)
             Assert.assertEquals("MLA805330648", it[1].id)
         }
     }
 
     @Test
-    fun getProductDetailsById_sentRequest_receivedExpected() = runBlocking{
-        val mockResponse = MockResponse()
-        mockResponse.setResponseCode(200)
-        mockWebServerRule.server.enqueue(mockResponse.fromJson("details.json"))
-        mockWebServerRule.server.enqueue(mockResponse.fromJson("description.json"))
+    fun getProductDetailsById_sentRequest_receivedExpected() = runBlocking {
+        val productId = "MLA805330648"
+        mockWebServerRule.server.dispatcher = SuccessDispatcher(productId)
 
-        val details = remoteDataSource.getProductDetailsById("MLA805330648")
+        val details = remoteDataSource.getProductDetailsById(productId)
 
         details.fold({ throw Exception(it.toString()) }) {
+            Assert.assertEquals(productId, it.id)
             Assert.assertEquals(15999, it.price)
             Assert.assertTrue(it.description.contains("1,8 GHz y 3 GB de RAM"))
         }
     }
 
+
+    @Test
+    fun verify_app_initialization() {
+        mockWebServerRule.server.dispatcher = SuccessDispatcher()
+
+        composeTestRule.clearAndSetContent{
+            MercadoLibreTheme {
+                Navigation()
+            }
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("search bar", useUnmergedTree = true)
+            .assertIsDisplayed()
+
+    }
+
+    /*
+
+    It seems there is a problem with performing text input in my SearchBar composable
+    because I'm getting the error:
+    "java.lang.NullPointerException: null cannot be cast to non-null
+    type androidx.compose.ui.platform.ViewRootForTest"
+
+    I've been trying everything but I haven't been able to solve it
+
+    @Test
+    fun test() = runTest {
+        mockWebServerRule.server.dispatcher = SuccessDispatcher()
+
+        composeTestRule.clearAndSetContent{
+            MercadoLibreTheme {
+                Navigation()
+            }
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("search bar", useUnmergedTree = true)
+            .performTextInput("hola")
+    }
+
+    @Test
+    fun test2() = runTest {
+        mockWebServerRule.server.dispatcher = SuccessDispatcher()
+
+        composeTestRule.clearAndSetContent{
+            MercadoLibreTheme {
+                Navigation()
+            }
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("search bar", useUnmergedTree = true)
+            .onChild()
+            .performTextInput("hola")
+    }
+
+
+    @Test
+    fun test3() = runTest {
+        mockWebServerRule.server.dispatcher = SuccessDispatcher()
+
+        composeTestRule.clearAndSetContent{
+            MercadoLibreTheme {
+                Navigation()
+
+            }
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("search bar", useUnmergedTree = true)
+            .performClick()
+
+        composeTestRule
+            .onNodeWithContentDescription("search bar", useUnmergedTree = true)
+            .performImeAction()
+    }
+
+
+    @Test
+    fun test4() = runTest {
+        mockWebServerRule.server.dispatcher = SuccessDispatcher()
+
+        composeTestRule.clearAndSetContent{
+            MercadoLibreTheme {
+                Navigation()
+            }
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("search bar", useUnmergedTree = true)
+            .onSiblings()[0]
+            .performTextInput("hola")
+    }
+
+    */
+}
+
+fun AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
+        .clearAndSetContent(content: @Composable () -> Unit) {
+    (this.activity.findViewById<ViewGroup>(android.R.id.content)
+        ?.getChildAt(0) as? ComposeView)?.setContent(content)
+        ?: this.setContent(content)
 }
--- app/src/main/java/com/lumec/challenge/mercadolibre/ui/product_list/components/SearchBar.kt
+++ app/src/main/java/com/lumec/challenge/mercadolibre/ui/product_list/components/SearchBar.kt
@@ -3,13 +3,15 @@ package com.lumec.challenge.mercadolibre.ui.product_list.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
@@ -21,18 +23,20 @@ fun SearchBar(
     onSearch: (String) -> Unit,
     onKeyboardSearchClicked: () -> Unit,
     onDelete: () -> Unit,
-    ) {
+) {
     Row(
-        modifier = Modifier
-                .fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        var text by remember { mutableStateOf(query)}
+        var text by remember { mutableStateOf(query) }
 
         Spacer(modifier = Modifier.width(8.dp))
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth().semantics {
-                this.contentDescription = "search bar" },
+            modifier = Modifier
+                .fillMaxWidth()
+                .semantics {
+                    this.contentDescription = "search bar"
+                },
             value = text,
             onValueChange = { text = it },
             placeholder = {
@@ -67,7 +71,7 @@ fun SearchBar(
             ),
             keyboardActions = KeyboardActions(
                 onSearch = {
-                    if(text.isNotEmpty()) {
+                    if (text.isNotEmpty()) {
                         onKeyboardSearchClicked()
                         onSearch(text)
                     }
